dc72521a72af47c7467719b05474888704bce03e
Mohammed Aljobory <mhdaljobory@gmail.com>
2011-11-20 15:25:44 +0300

    Set highlight of project

--- Temp/src/OR/Condition.java
+++ Temp/src/OR/Condition.java
@@ -6,5 +6,48 @@ package OR;
  * 
  */
 public class Condition {
+    //is the Condition Large or Less
+    private boolean isLarge; 
     
+    //The Number of Variables in Condition
+    private int numberOfVariable;
+    
+    //Constant of every variables
+    private double[] constant;
+    
+    //Second side of Condition
+    private double secondSide;
+    
+    //The Equation of Condition
+    private Equation equation;
+    
+    public void Condition(String condition) {
+        //TODO Parsing condition and put it in the variables
+    }
+    
+    private void addSurplus() {
+        //Add surplus variables
+    }
+    
+    private void addSlack() {
+        //Add slack variables
+    }
+    
+    private void addR() {
+        
+    }
+    
+    public void toEquation() {
+        if (isLarge) {
+            //Add surplus variable
+            addSurplus();
+        }
+        else {
+            //Add slack variable
+            addSlack();
+            //Add R
+            addR();
+        }
+        equation = new Equation(constant,numberOfVariable);
+    }
 }
--- Temp/src/OR/Equation.java
+++ Temp/src/OR/Equation.java
@@ -6,5 +6,14 @@ package OR;
  * 
  */
 public class Equation {
+    //The Number of variables in Equation
+    private int numberOfVariable;
     
+    //Constant of every variables
+    private double[] constant;
+
+    public Equation(double[] constant, int numberOfVariable) {
+        this.constant = constant;
+        this.numberOfVariable = numberOfVariable;
+    }
 }
