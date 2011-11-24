f9ed5e636919c631ae407eb8f4d583d80259d539
Mohammed Aljobory <mhdaljobory@gmail.com>
2011-11-24 21:55:03 +0300

    Set inVariable instead of outVariable

--- Temp/src/OR/SimplexTable.java
+++ Temp/src/OR/SimplexTable.java
@@ -38,7 +38,18 @@ public class SimplexTable {
         this.numberOfNonBasicVariables = numberOfNonBasicVariables;
     }
     
+    private int getIndexOfVariable(int basicVariable) {
+        for (int i = 0; i < Basic.length; i++) {
+            if (Basic[i] == basicVariable) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
     public void updateTable(int outVariable,int inVariable) {
+        //Entry inVariables instead of outVariable
+        Basic[getIndexOfVariable(outVariable)] = inVariable;
         //if it's the same line a' = a/b
         //else a' = a - ((cb)/d)
     }
