b370136ede417597ac0878c2b0aa1d292ac81d0a
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2011-11-25 19:05:04 +0200

    some constructors

--- Temp/src/OR/SimplexProblem.java
+++ Temp/src/OR/SimplexProblem.java
@@ -1,10 +1,43 @@
 
 package OR;
 
+import Jama.Matrix;
+
 /**
  *
  * 
  */
 public class SimplexProblem {
+    Matrix A;
+    Matrix C;
     
+    /**
+     * a constructor for internal uses only
+     */
+    private SimplexProblem() {
+        
+    }
+    
+    public SimplexProblem(double[][] A,double[] C , double[] b) {
+       // we consider that all conditions are <=  .... simple simplex
+        
+       //creating Matrix[m,n+m]; slack for every condition
+       int m = A.length;
+       int n = A[0].length ;
+       Matrix mA = new Matrix(m, n + m);
+       
+       int[] width = new int[m];
+       for (int i=0;i<m;i++) width[i] = i;
+       //extends the matrix by an identity matrix of slacks
+       mA.setMatrix(n, n+m-1,new int[m], Matrix.identity(m, m));
+       this.A = mA;
+       //this.C = new Matrix;
+    }
+    
+    /**
+     * method for creating Simplex Problem by Entering the Basic Form
+     */
+    public static SimplexProblem fromBasicFrom() {
+        return new SimplexProblem();
+    }
 }
